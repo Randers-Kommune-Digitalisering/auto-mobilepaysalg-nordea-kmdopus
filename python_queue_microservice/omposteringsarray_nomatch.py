c435965730db5d6fa18260fa216666e214082fb0
Christian Leonhardt <121159190+CSL-randers@users.noreply.github.com>
2023-10-26 12:55:47 +0000

    .

--- python_queue_microservice/omposteringsarray_nomatch.py
+++ python_queue_microservice/omposteringsarray_nomatch.py
@@ -24,6 +24,7 @@ def preprocess_text(text):
 def transform(data_in):
     bankposteringer = json.loads(data_in)
     print(bankposteringer)
+    print(bankposteringer[0])
     new_data = []
 
     # for postering in bankposteringer:
